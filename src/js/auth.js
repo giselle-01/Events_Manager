68228d52ea96a051e532eea9cdd00ed4ee24abdc
giselle-01 <giselleandrea027@gmail.com>
2025-07-14 15:48:58 -0500

    Feat: the api file was done.

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,11 +1,40 @@
 
+import { api } from './api.js';
+
 export const auth = {
-    login: async (email, pass) => {
+    login: async (email, password) => {
         const users = await api.get (`/users?email=${email}`);
-        if (users.length === 0 || users[0] .password !== pass) {
-            throw new Error('Credenciales inválidas');
+        if (users.length === 0 || users[0].password !== password) {
+            throw new Error('Email y contraseña inválida');
         }
         const user = users[0];
         localStorage.setItem('users', JSON.stringify(user));
+    },
+
+    register: async (name, email, password) => {
+        const existingUser = await api.get(`/users?email=${email}`);
+        if (existingUser.length > 0) {
+            throw new Eror('El usuario ya está registrado');
+        }
+        const newUser = {name, email, pasword: password};
+        await api.post('/users', newUser);
+    },
+
+    logout: () => {
+        localStorage.removeItem('user');
+    },
+
+    isAuthenticated: () => {
+        return !!localStorage.getItem('user');
+    },
+
+    getUser: () => {
+        const user = localStorage.getItem('user');
+        if (user) {
+            return JSON.parse(user);
+        } else {
+            return null
+        }
     }
+
 };
